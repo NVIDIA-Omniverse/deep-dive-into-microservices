531361cff8cbac07eb5271d055393d5a2b8c7132
Andrew Grant <agrant@nvidia.com>
2023-01-03 20:32:43 -0500

    start http server

--- start_service.bat
+++ start_service.bat
@@ -1,2 +1,2 @@
-REM Launch Create, with the extension enabled:
-call %~dp0\app\kit\kit.exe --ext-folder  %~dp0\app\exts-farm-queue --ext-folder ./exts --enable omni.services.assets.convert
+REM Launch Kit, with the extension enabled and transport server:
+call %~dp0\app\kit\kit.exe --ext-folder  %~dp0\app\exts-farm-queue --ext-folder ./exts --enable omni.services.transport.server.http --enable omni.services.assets.convert --enable omni.services.assets.validate %*
